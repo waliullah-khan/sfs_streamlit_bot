ad9a1f2da363af6452eb19d0a46cf13cd0753727
Vladimir Timofeenko <vladimir.timofeenko@snowflake.com>
2024-12-17 16:08:56 -0800

    Set STREAMLIT location as in the instructions
    The instructions tell the user to upload all files to the root of the
    stage. The setup_script creates STREAMLIT from /src/ subdirectory.
    
    Without this change, application is created but fails to start.
    
    Alternative is to instruct the user to upload the files to a
    subdirectory but that would involve specifying `src` as the path when
    uploading files.

--- app/setup_script.sql
+++ app/setup_script.sql
@@ -110,7 +110,7 @@ GRANT USAGE ON PROCEDURE core.register_single_callback( STRING,  STRING,  STRING
 -- 5. Create a streamlit object using the code you wrote in you wrote in src/module-ui, as shown below.
 -- The `from` value is derived from the stage path described in snowflake.yml
 CREATE OR REPLACE STREAMLIT core.ui
-     FROM '/src/'
+     FROM '/'
      MAIN_FILE = 'ui.py';
 
 -- 6. Grant appropriate privileges over these objects to your application roles.
